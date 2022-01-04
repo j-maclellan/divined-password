aab02eea99c294a441f39115329ac4118f718e74
Laertes <maclellanj15@gmail.com>
2022-01-04 00:28:09 -0500

    began password length, and password criteria functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,32 @@
 // Assignment Code
+// available characters for password
+var char = {
+  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowerCase:  "abcdefghijklmnopqrstuvwxyz",
+  number: "0123456789",
+  symbol: "!@#$%^&*()<>",
+};
 
-var generateBtn = document.querySelector("#generate");
+// random length for password, needs work
+function passwordLength(min, max) {
+  var length = Math.floor(Math.random() * (max - min + 1) + min);
+  console.log("Your password length is: " + length + "!");
+  return length;
+}
+
+
+
+var generatePassword = function() {
+    // passwordLength(8, 128);
+    // conditional password confirms
+    if (confirm("Would you like uppercase letters in your password?") == true) {
+        return char.upperCase[Math.floor(Math.random() * char.upperCase.length)];
+      }
+
+    char.length();
+  }
+  var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -8,7 +34,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
