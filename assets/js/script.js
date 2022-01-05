5e94fec17567cbe3ef35912441b44a2975f73419
Laertes <maclellanj15@gmail.com>
2022-01-04 22:41:36 -0500

    made var char into arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,19 @@
 // Assignment Code
 // available characters for password
-let char = [
+// var char = [
   
-  upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  lowerCase =  "abcdefghijklmnopqrstuvwxyz",
-  number = "0123456789",
-  symbol = "!@#$%^&*()<>",
-  // all: upperCase + lowerCase + number + symbol,
-];
+//   upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+//   lowerCase =  "abcdefghijklmnopqrstuvwxyz",
+//   number = "0123456789",
+//   symbol = "!@#$%^&*()<>",
+//   // all: upperCase + lowerCase + number + symbol,
+// ];
+const upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+const lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const number = ['0','1','2','3','4','5','6','7','8','9'];
+const symbol = ['!','@','#','$','%','^','&','*','(',')','_','+','<','>','?','{','}','[',']','~'];
+
+// const randomUpper = Math.floor(Math.random() * upperCase.length);
 
 // random length for password, needs work
 function passwordRange(userInput, min, max) {
@@ -29,24 +35,27 @@ var generatePassword = function() {
     passwordRange("", 8, 128);
     // conditional password confirms
     var upperYes = confirm("Would you like uppercase letters in your password?");
-    var lowerYes = confirm("Would you like lowercase letters in your password?");
-    var numYes = confirm("Would you like numbers in your password?");
-    var symYes = confirm("Would you like symbols in your password?");
     if (upperYes == true) {
-        return upperCase.conc;
-      }
-    if (lowerYes == true) {
-      return char.lowerCase[Math.floor(Math.random() * passwordRange.length)];
-      }
-    if (numYes == true) {
-      return char.number[Math.floor(Math.random() * passwordRange.length)];
+      return Math.floor(Math.random() * upperCase.value);
     }
-    if (symYes == true) {
-      return char.symbol[Math.floor(Math.random() * passwordRange.length)];
-    }
-    if (upperYes == true && lowerYes == true) {
+    // var lowerYes = confirm("Would you like lowercase letters in your password?");
+    // var numYes = confirm("Would you like numbers in your password?");
+    // var symYes = confirm("Would you like symbols in your password?");
+    // if (upperYes == true) {
+    //     return upperCase[Math.floor(Math.random() * passwordRange.length)];
+    //   }
+    // if (lowerYes == true) {
+    //   return char.lowerCase[Math.floor(Math.random() * passwordRange.length)];
+    //   }
+    // if (numYes == true) {
+    //   return char.number[Math.floor(Math.random() * passwordRange.length)];
+    // }
+    // if (symYes == true) {
+    //   return char.symbol[Math.floor(Math.random() * passwordRange.length)];
+    // }
+    // if (upperYes == true && lowerYes == true) {
 
-    }
+    // }
     
   }
   var generateBtn = document.querySelector("#generate");
