fab70381f88e4db6a4c0cba33271fb501ff61b61
Laertes <maclellanj15@gmail.com>
2022-01-04 22:10:35 -0500

    added conditionals

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,53 @@
 // Assignment Code
 // available characters for password
-var char = {
-  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  lowerCase:  "abcdefghijklmnopqrstuvwxyz",
-  number: "0123456789",
-  symbol: "!@#$%^&*()<>",
-};
+let char = [
+  
+  upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowerCase =  "abcdefghijklmnopqrstuvwxyz",
+  number = "0123456789",
+  symbol = "!@#$%^&*()<>",
+  // all: upperCase + lowerCase + number + symbol,
+];
 
 // random length for password, needs work
-function passwordLength(min, max) {
-  var length = Math.floor(Math.random() * (max - min + 1) + min);
-  console.log("Your password length is: " + length + "!");
-  return length;
+function passwordRange(userInput, min, max) {
+  var userInput = window.prompt("Please enter the number of characters in password, between 8 and 128.");
+  console.log(userInput);
+
+  if (userInput >= min && userInput <= max)  {
+    userInput.value = passwordRange;
+    return true;
+  } else {
+    alert("Please input between " + min + " and " + max + " characters.");
+    return passwordRange();
+  }
 }
 
 
 
 var generatePassword = function() {
-    // passwordLength(8, 128);
+    passwordRange("", 8, 128);
     // conditional password confirms
-    if (confirm("Would you like uppercase letters in your password?") == true) {
-        return char.upperCase[Math.floor(Math.random() * char.upperCase.length)];
+    var upperYes = confirm("Would you like uppercase letters in your password?");
+    var lowerYes = confirm("Would you like lowercase letters in your password?");
+    var numYes = confirm("Would you like numbers in your password?");
+    var symYes = confirm("Would you like symbols in your password?");
+    if (upperYes == true) {
+        return upperCase.conc;
       }
+    if (lowerYes == true) {
+      return char.lowerCase[Math.floor(Math.random() * passwordRange.length)];
+      }
+    if (numYes == true) {
+      return char.number[Math.floor(Math.random() * passwordRange.length)];
+    }
+    if (symYes == true) {
+      return char.symbol[Math.floor(Math.random() * passwordRange.length)];
+    }
+    if (upperYes == true && lowerYes == true) {
 
-    char.length();
+    }
+    
   }
   var generateBtn = document.querySelector("#generate");
 
