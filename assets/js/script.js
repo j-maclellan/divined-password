2902e888ef5492739bcca40714e919a5e4a37178
Laertes <maclellanj15@gmail.com>
2022-01-05 23:11:07 -0500

    made output for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,56 @@
 // Assignment Code
 // available characters for password
-// var char = [
-  
-//   upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-//   lowerCase =  "abcdefghijklmnopqrstuvwxyz",
-//   number = "0123456789",
-//   symbol = "!@#$%^&*()<>",
-//   // all: upperCase + lowerCase + number + symbol,
-// ];
+
 const upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 const lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 const number = ['0','1','2','3','4','5','6','7','8','9'];
 const symbol = ['!','@','#','$','%','^','&','*','(',')','_','+','<','>','?','{','}','[',']','~'];
 
-// const randomUpper = Math.floor(Math.random() * upperCase.length);
 
-// random length for password, needs work
-function passwordRange(userInput, min, max) {
-  var userInput = window.prompt("Please enter the number of characters in password, between 8 and 128.");
+
+// length for password
+function passwordRange() {
+  var userInput = window.prompt("Please enter the number of characters in your password, between 8 and 128.");
   console.log(userInput);
 
-  if (userInput >= min && userInput <= max)  {
-    userInput.value = passwordRange;
-    return true;
+  if (userInput >= 8 && userInput <= 128)  {
+    return userInput;
   } else {
-    alert("Please input between " + min + " and " + max + " characters.");
-    return passwordRange();
+    alert("Please input between 8 and 128 characters.");
+    return passwordRange;
   }
 }
 
-
-
 var generatePassword = function() {
-    passwordRange("", 8, 128);
+    var input = passwordRange();
     // conditional password confirms
     var upperYes = confirm("Would you like uppercase letters in your password?");
+    var lowerYes = confirm("Would you like lowercase letters in your password?");
+    var numYes = confirm("Would you like numbers in your password?");
+    var symYes = confirm("Would you like symbols in your password?");
+    var confirms = [];  
     if (upperYes == true) {
-      return Math.floor(Math.random() * upperCase.value);
+        confirms.concat(upperCase);
     }
-    // var lowerYes = confirm("Would you like lowercase letters in your password?");
-    // var numYes = confirm("Would you like numbers in your password?");
-    // var symYes = confirm("Would you like symbols in your password?");
-    // if (upperYes == true) {
-    //     return upperCase[Math.floor(Math.random() * passwordRange.length)];
-    //   }
-    // if (lowerYes == true) {
-    //   return char.lowerCase[Math.floor(Math.random() * passwordRange.length)];
-    //   }
-    // if (numYes == true) {
-    //   return char.number[Math.floor(Math.random() * passwordRange.length)];
-    // }
-    // if (symYes == true) {
-    //   return char.symbol[Math.floor(Math.random() * passwordRange.length)];
-    // }
-    // if (upperYes == true && lowerYes == true) {
-
-    // }
-    
-  }
+    if (lowerYes == true) {
+      confirms.push(lowerCase);
+    }
+    if (numYes == true) {
+      confirms.push(number);
+    }
+    if (symYes == true) {
+      confirms.push(symbol);
+    }
+    // else {
+    //   alert("Please choose at least one parameter");
+    //  return generatePassword();
+    // };
+    var output = "";
+for (var i = 0; i < input; i++) {
+      output += confirms[Math.floor(Math.random() * input)];
+    }
+    return output;
+};
   var generateBtn = document.querySelector("#generate");
 
 
